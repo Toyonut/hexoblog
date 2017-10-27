7ba64696e276b00019f8048b67925d13c82f7430
Paul Milbank <paulmilbank@gmail.com>
2017-10-27 22:55:15 +1300

    Add  a couple of build switches

--- build.ps1
+++ build.ps1
@@ -1,9 +1,32 @@
-if (Get-Command -Name "hexo" -ErrorAction SilentlyContinue) {
+[CmdletBinding()]
+param (
+    [switch]$build,
+    [switch]$deploy,
+    [switch]$serve
+)
+
+if (!(Get-Command -Name "hexo" -ErrorAction SilentlyContinue)) {
+    write-error "Hexo not installed." -ErrorAction Stop
+}
+
+if ($build) {
     Write-Host "Cleaning Folders" -ForegroundColor Green
     hexo clean
 
-    Write-Host "Building and deploying" -ForegroundColor Green
+    Write-Host "Building" -ForegroundColor Green
+    hexo generate
+}
+
+if ($deploy) {
+    Write-Host "Cleaning Folders" -ForegroundColor Green
+    hexo clean
+
+    Write-Host "Deploying" -ForegroundColor Green
     hexo deploy
-} else {
-    Write-Error "Hexo not installed"
+}
+
+if ($serve) {
+    start-process hexo serve
+    start-sleep -Seconds 3
+    start-process "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "http://localhost:4000"
 }
